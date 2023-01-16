75be5101ad9d639df60805d5ab55fbc784a9c1c3
ags3780 <hammerhead12as@icloud.com>
2023-01-16 13:34:46 -0600

    arm and claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTesting.java
@@ -61,6 +61,13 @@ public class DriveTesting extends OpMode
     private DcMotorEx wheelFR;
     private DcMotorEx wheelBL;
     private DcMotorEx wheelBR;
+    private double speedMod;
+    private double previousRunTime;
+    private double inputDelayInSeconds = .5;
+    private boolean rumbleLevel = true;
+    private boolean gripperToggle = false;
+    private double rotation = 0;
+    //double susanPower;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -72,7 +79,7 @@ public class DriveTesting extends OpMode
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        wheelFL  = hardwareMap.get(DcMotorEx.class, "wheelFL";
+        wheelFL  = hardwareMap.get(DcMotorEx.class, "wheelFL");
         wheelFR  = hardwareMap.get(DcMotorEx.class, "wheelFR");
         wheelBL  = hardwareMap.get(DcMotorEx.class, "wheelBL");
         wheelBR  = hardwareMap.get(DcMotorEx.class, "wheelBR");
@@ -114,40 +121,37 @@ public class DriveTesting extends OpMode
     /*
      * Code to run ONCE when the driver hits PLAY
      */
+
     @Override
     public void start() {
         runtime.reset();
+        previousRunTime = getRuntime();
     }
-
-    /*
+        /*
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
      */
     @Override
     public void loop() {
+        drivingControl2();
+    }
+    public void drivingControl2(){
+        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
 
-        //Get game controller input
-        double r = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
 
-        //make calculations based upon the input
-        double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-        double rightX = -gamepad1.right_stick_x;
-        final double v1 = r * Math.cos(robotAngle) - rightX;
-        final double v2 = r * Math.sin(robotAngle) + rightX;
-        final double v3 = r * Math.sin(robotAngle) - rightX;
-        final double v4 = r * Math.cos(robotAngle) + rightX;
-
-        //change the power for each wheel
-        wheelFL.setPower(-v1);
-        wheelFR.setPower(-v2);
-        wheelBL.setPower(v3);
-        wheelBR.setPower(v4);
-
-
-
-
-
-
-
+        wheelFL.setPower(frontLeftPower);
+        wheelBL.setPower(backLeftPower);
+        wheelFR.setPower(frontRightPower);
+        wheelBR.setPower(backRightPower);
     }
 
     /*
